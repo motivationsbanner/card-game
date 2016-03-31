2a5f83a6f80d4f7f6ea18a4327930350abf2ea27
Tim Jahn <ceoymw@gmail.com>
2016-03-31 23:34:23 +0200

    Multiple Games Support
    Added:
    - Multiple Games
    - New Queue after Opponent Disconnect
    - UM?..xD aah yea, formatting

--- game_server.js
+++ game_server.js
@@ -7,13 +7,13 @@ var http = require('http'),
 	functions = require('functions.js'),
 	game_function = require('game_function.js')
 	
-// Games array
-games = game_function.game();
+// Games and Players Array
+games = game_function.GameMethods("games");
+players = game_function.GameMethods("players");
 
 // Some cool variables
 var ppl = 0;
 var am_zug, nicht_am_zug;
-games.push(game_function.game());
 
 // Send index.html to all requests
 var app = http.createServer(function(req, res) {
@@ -27,32 +27,44 @@ app.listen(8000, function(){
   console.log('listening on *:8000');
 });
 
-io.on('connection', function(socket)
+io.sockets.on('connection', function(client)
 {	
+	var game;
 	ppl++;
-
+	players.add(client);
+	// Check if there are enough players waiting
+	game = players.checkRdy();
+	
+	if (game != false)
+		startGame(game);
+	else
+		client.emit('system', 'Waiting for another Player.  ¯\\_(ツ)_/¯  We are sorry.');
+	
+	/*
 	// Check whether or not we can create a new active Player
-	// Parameter: socket, so we know who we're talking to:)
+	// Parameter: client, so we know who we're talking to:)
 	// sidenote: atm there is no need for that check.
-	if (!newPlayer(socket))
-		socket.emit('system', 'Game full atm.  ¯\\_(ツ)_/¯  We are sorry.');
+	if (!newPlayer(client))
+		client.emit('system', 'Game full atm.  ¯\\_(ツ)_/¯  We are sorry.');
 	else
 	{
 		// Checks if there are 2 players waiting
 		if (checkGame())
 			startGame(); // The Game just got full -> we can start playing
 		else
-			socket.emit('system', 'Waiting for another Player ┗(＾0＾)┓ ');
+			client.emit('system', 'Waiting for another Player ┗(＾0＾)┓ ');
 	}
-
+	*/
+	
+	
 	// We could just comment the 'message' block out.. but lets leave it here.
-	socket.on('message', function(msg)
+	client.on('message', function(msg)
 	{
 		msg.username = strip(msg.username);
 		msg.chat = strip(msg.chat);
 		
 		console.log(msg.username + " " + msg.chat);
-		socket.broadcast.emit('message', msg);
+		client.broadcast.emit('message', msg);
 		
 		// Checks if the message send is a chatcommand
 		if (functions.isCommand(msg.chat) )
@@ -68,13 +80,36 @@ io.on('connection', function(socket)
 	});
 	
 
-	socket.on('disconnect', function()
+	client.on('disconnect', function()
 	{
-		socket.broadcast.emit('system', 'Someone disconnected');
+		client.broadcast.emit('system', 'Someone disconnected');
+		if ( players.players.indexOf( client ) != -1 )
+			players.players.splice( players.players.indexOf( client ), 1);
+		else
+			clean ( getGameByClient(client) , client );
 		ppl--;
 	});
 });
+/*
+io.sockets.on('connection', function (socket) {
+    socket.on('disconnect', function () {
+        console.log(socket.id);
+    });
+});
+*/
 
+function clean (gameID, client)
+{
+	players.add( getLastPlayerInGame( getGameByClient(client) , client ) ); 
+	games.remove( getGameByClient (client) );
+	
+	var game = players.checkRdy();
+	
+	if (game != false)
+		startGame(game);
+	else
+		client.emit('system', 'Waiting for another Player.  ¯\\_(ツ)_/¯  We are sorry.');
+}
 
 // Creates a new Player
 // true if successful; false if not
@@ -99,6 +134,7 @@ function newPlayer (id)
 }
 
 // Checks if there are 2 player waiting for a game
+// Outdated
 function checkGame()
 {
 	return (am_zug == games[getGameByPlayer(am_zug)].player1 && nicht_am_zug == games[getGameByPlayer(nicht_am_zug)].player2) ? true : false;
@@ -106,11 +142,18 @@ function checkGame()
 
 // Starts the game by giving each player 3 cards
 // TODO: Telling player1 that his turn stards now.
-function startGame()
+function startGame(game)
 {
+	// OLD CODE
+	/*
 	system_message('THE GAME HAS BEGUN!');
-	nicht_am_zug.socket.emit('draw', nicht_am_zug.draw(3));
-	am_zug.socket.emit('draw', am_zug.draw(3));
+	nicht_am_zug.client.emit('draw', nicht_am_zug.draw(3));
+	am_zug.client.emit('draw', am_zug.draw(3));
+	*/
+
+	games.add(game);
+	console.log(games.games.length);
+	system_message("Start game");
 }	
 
 // HTMLSPECIALCHAR
@@ -136,13 +179,38 @@ function createEmptyGame()
 }
 
 
+function getLastPlayerInGame(gameID, player)
+{
+	console.log("getLastPlayerInGame gameID: " + gameID);
+	var p1 = games.games[gameID].player1.getClientID();
+	var p2 = games.games[gameID].player2.getClientID();
+	if ( p1 == player )
+		return p2;
+	if ( p2 == player )
+		return p1;
+}	
 function getGameByPlayer(player)
 {
 	return player.gameID;
 }
 
-// Still need to implement this
-function getEmptyGameId()
+function getGameByClient(client)
 {
-	// some code
+	for ( i = 0; i < games.games.length; i++ )
+	{	
+		var game = games.games[i];
+		console.log(game);
+		if ( game.player1.getClientID() == client )
+		{
+			return i;
+			break;
+		}
+		if ( game.player2.getClientID() == client )
+		{
+			return i;
+			break;
+		}
+		console.log("getGameByClient Loop : " + i);
+	}
+	return -1;
 }

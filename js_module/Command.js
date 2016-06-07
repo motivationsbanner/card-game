e0021885eedc935c28fdc7eed7cd170e4d0792fc
Tim Jahn <ceoymw@gmail.com>
2016-06-07 10:36:05 +0200

    Update Command.js

--- js_module/Command.js
+++ js_module/Command.js
@@ -24,6 +24,7 @@ function select_optionCommand(data, game)
 {
 	if ( game.getOnTurn().getSelectedCard() == data.pos)
 	{
+		console.log("yay");
 		game.getOnTurn().setSelectedCard(-1);
 		game.playOptions();
 	}

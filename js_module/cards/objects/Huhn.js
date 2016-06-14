c651480dfc39757824eb7e2a1993f65ace5e54a8
timmmmmb <transotum@hispeed.ch>
2016-06-14 10:52:19 +0200

    added class Huhn

---
+++ js_module/cards/objects/Huhn.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+var text = "Ein Huhn ist fett und bereit um geschlachtet zu werden.";
+var health = 2;
+var attack = 0;
+
+var MeleeMinion = require(__dirname + '/../MeleeMinion.js');
+
+class Huhn extends MeleeMinion {
+	constructor ()
+	{
+		super();
+	}
+}
+
+Soldat.nom = "Huhn"
+Soldat.text = text;
+Soldat.health = health;
+Soldat.attack = attack;
+
+module.exports = Huhn;

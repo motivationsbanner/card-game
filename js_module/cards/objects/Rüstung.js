aae849e8dd55c91eef278746fb650fcbd93382e5
Tim Jahn <ceoymw@gmail.com>
2016-06-19 17:01:38 +0200

    fixed glow

---
+++ js_module/cards/objects/Rüstung.js
@@ -0,0 +1,43 @@
+"use strict";
+var voraussetzung = "yea";
+var text = "Frisch aus der Eisenschmiede. +2 Health";
+var health = 0;
+var attack = 0;
+
+var spell = require(__dirname + '/../Spell.js');
+
+class Rüstung extends spell {
+	constructor ()
+	{
+		super();
+	}
+	
+	isPlayable(c)
+	{
+		if (super.isPlayable(c))
+		{
+			// YOUR CONDITIONS
+			return c.maxAmountPlayerMinions(1);
+			
+		}
+		return false;
+	}
+	
+	getPlayableFields(field)
+	{
+		return field.getFieldCards("Player");
+	}
+	
+	activate (target, manipulator)
+	{
+		manipulator.buffHP(target, 2);
+	}
+}
+Rüstung.voraussetzung = voraussetzung;
+Rüstung.nom = "Rüstung";
+Rüstung.text = text;
+Rüstung.health = health;
+Rüstung.attack = attack;
+
+
+module.exports = Rüstung;
